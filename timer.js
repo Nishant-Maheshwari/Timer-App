80ed1bb370e870a8a2785c500239a1a8ece956a6
Nishant Maheshwari <nishant2622@gmail.com>
2025-09-01 18:23:12 +0530

    add hours minutes and seconds formate

--- timer.js
+++ timer.js
@@ -1,18 +1,54 @@
-let timeInput = document.querySelector('.js-timer')
+// let timeInput = document.querySelector('.js-sec')
+// let startButton = document.querySelector('.start')
+// let h1 = document.createElement("h1")
+// document.body.append(h1)
+
+// startButton.addEventListener('click',() => {
+//   let timeleft = Number(timeInput.value)
+//   let intervalId = setInterval(()=>{
+//     timeleft--;
+//     h1.textContent = `The timer will end after ${timeleft} seconds`
+
+//     if(timeleft <= 0){
+//       clearInterval(intervalId)
+//       h1.textContent = `The countdown is over`
+//     }
+    
+//   },1000)
+  
+
+// timeInput.value = ""
+// }
+// )
+let timeInputHr = document.querySelector('.js-hr')
+let timeInputMin = document.querySelector('.js-min')
+let timeInputSec = document.querySelector('.js-sec')
 let startButton = document.querySelector('.start')
 let h1 = document.createElement("h1")
-document.body.append(h1)
-startButton.addEventListener('click',() => {
-  let timeleft = Number(timeInput.value)
-  let intervalId = setInterval(()=>{
-    timeleft--;
-    h1.textContent = `The timer will end after ${timeleft} seconds`
+document.body.append(h1) 
 
-    if(timeleft <= 0){
-      clearInterval(intervalId)
-      h1.textContent = `The countdown is over`
-    }
-    
+startButton.addEventListener('click',()=>{
+let hours = Number(timeInputHr.value)
+let minutes = Number(timeInputMin.value)
+let seconds = Number(timeInputSec.value) 
+
+  let timeLeft = (hours*3600) + (minutes*60) + seconds;
+  let intervalId = setInterval(()=>{
+  timeLeft--;
+  let hrs = Math.floor(timeLeft/3600)
+  let mins = Math.floor((timeLeft % 3600) / 60)
+  let secs = Math.floor(timeLeft%60) 
+  console.log(hrs);
+  console.log(mins);
+  console.log(secs);
+  
+
+  h1.textContent = `The timer will end after ${hrs}:${mins}:${secs} seconds`;
+  
+  if(timeLeft <= 0){
+    clearInterval(intervalId)
+    h1.textContent = `the countdown is over`
+  }
   },1000)
   
-}) 
+})
