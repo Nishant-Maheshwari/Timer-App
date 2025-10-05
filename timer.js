d55eec83e5e8646a1b47ca16efe1c3660c215137
Nishant Maheshwari <nishant2622@gmail.com>
2025-10-05 15:59:25 +0530

    Implemented closures & modular functions for timer/stopwatch app

--- timer.js
+++ timer.js
@@ -1,139 +1,146 @@
-// let timeInput = document.querySelector('.js-sec')
-// let startButton = document.querySelector('.start')
-// let h1 = document.createElement("h1")
-// document.body.append(h1)
+let mainDiv =document.querySelector('.js-mode');
+let modeBtton = document.createElement('button');
+let timeDisplay = document.createElement('h1');
+let stopwatchDisplay = document.createElement('h1');
+let flagDisplay = document.createElement('h2')
+let isTimerRunning = true;
+modeBtton.textContent = "Mode Change";
+document.body.append(modeBtton,timeDisplay,stopwatchDisplay,flagDisplay) 
 
-// startButton.addEventListener('click',() => {
-//   let timeleft = Number(timeInput.value)
-//   let intervalId = setInterval(()=>{
-//     timeleft--;
-//     h1.textContent = `The timer will end after ${timeleft} seconds`
+modeBtton.addEventListener('click',()=>{
+isTimerRunning = !isTimerRunning;
+console.log(isTimerRunning);
+render()
 
-//     if(timeleft <= 0){
-//       clearInterval(intervalId)
-//       h1.textContent = `The countdown is over`
-//     }
-    
-//   },1000)
-  
-
-// timeInput.value = ""
-// }
-// )
-let isTimerMode = true
-let mainDiv = document.querySelector('.js-mode')
-let timeInputHr = document.querySelector('.js-hr')
-let timeInputMin = document.querySelector('.js-min')
-let timeInputSec = document.querySelector('.js-sec')
-let startButton = document.querySelector('.start')
-let pauseButton = document.querySelector('.pause')
-let h1 = document.createElement("h1")
-let modeChangeBtn = document.createElement('button')
-document.body.append(h1) 
-modeChangeBtn.textContent = "Mode Change"
-document.body.append(modeChangeBtn) 
-let intervalId;
-let isRunning = false
-let timeLeft;
-
-startButton.addEventListener('click',()=>{
-  let hours = Number(timeInputHr.value)
-let minutes = Number(timeInputMin.value)
-let seconds = Number(timeInputSec.value) 
-
-  timeLeft = (hours*3600) + (minutes*60) + seconds;
-runningTimer()
-  
-}) 
-pauseButton.addEventListener('click',()=>{
-  if(isRunning === true ){
-    pauseButton.textContent = "Resume"
-    clearInterval(intervalId)
-    isRunning = false;
-  }else{
-    pauseButton.textContent = "Pause"
-    runningTimer()
-  }
 }) 
 
-function runningTimer(){  
-isRunning = true;
-
-  intervalId = setInterval(()=>{
-  timeLeft--;
-  let hrs = Math.floor(timeLeft/3600)
-  let mins = Math.floor((timeLeft % 3600) / 60)
-  let secs = Math.floor(timeLeft%60) 
-  console.log(hrs);
-  console.log(mins);
-  console.log(secs);
-  
-
-  h1.textContent = `The timer will end after ${hrs}:${mins}:${secs} seconds`;
-  
-  if(timeLeft <= 0){
-    clearInterval(intervalId)
-    h1.textContent = `the countdown is over`
-  }
-  },1000)} 
-
-  function render(){
-if(!isTimerMode){
-  mainDiv.innerHTML = `
-  <button>Start</button>
-  <button>stop</button>`
-} else {
-  mainDiv.innerHTML = `
-   <input type="number" class="js-hr" placeholder="Input in hours">
+function render(){
+  if(isTimerRunning){
+    mainDiv.innerHTML = `
+  <input type="number" class="js-hr" placeholder="Input in hours">
   <input type="number" class="js-min" placeholder="Input in minutes">
   <input type="number" class="js-sec" placeholder="Input in seconds">
   <button class="start">Start Timer</button>
   <button class="pause">pause</button>
-  <script src="timer.js"></script>
-  `
-}
-  }
-
-
-modeChangeBtn.addEventListener('click',()=>{
-  if(!isTimerMode){
-    isTimerMode = true;
+  <button class="reset">Reset</button>
+    `
+    runningTimer()
   }else{
-    isTimerMode = false
+     mainDiv.innerHTML = `
+   <button class= "start2">Start</button>
+   <button class = "stop2">stop</button>
+   <button class = "reset">Reset</button>
+   <button class = "lap">laps</button> `
+    runningStopWatch()
   }
-  render()
-})
+} 
+///formate time display///
+function formateTime(elapsed){
+  let hrs = Math.floor(elapsed/3600000);
+  let mins = Math.floor(elapsed%3600000/60000);
+  let secs = Math.floor(elapsed%60000/1000)
+  let ms = elapsed%1000;
+  hrs =  String(hrs).padStart(2,"0");
+  mins = String(mins).padStart(2,"0");
+  secs = String(secs).padStart(2,"0");
+  ms  =  String(ms).padStart(3,"0"); 
+  
+  return  `${hrs}:${mins}:${secs}:${ms}`
+}
+////Timer////
+function runningTimer(){
+  let elapsed = 0;
+  let intervalId;
 
-//   /////stopwatch///// 
-// // let h1Again = createElement('h1')
-// // document.body.append(h1Again)
-// let modeBtton = document.createElement('button')
-// modeBtton.textContent = `Mode change`
-// mainDiv.append(modeBtton)
-// let intervalId2 
-// let stopWatchTime = 0
-// modeBtton.addEventListener('click',()=>{
+
+  ///start function///
+  function start(){
+    ////selectors////
+  let HrsInput = document.querySelector('.js-hr').value;
+  let MinsInput = document.querySelector('.js-min').value;
+  let SecInput = document.querySelector('.js-sec').value;
+  elapsed = (HrsInput * 3600000) + (MinsInput * 60000) + (SecInput * 1000) 
+  console.log(elapsed); 
+
+    intervalId = setInterval(()=>{
+      elapsed -= 10;
+      timeDisplay.innerHTML = `Time remaining : ${formateTime(elapsed)}`
+
+       if(elapsed <= 0){
+      clearInterval(intervalId)
+      timeDisplay.innerHTML = "Time's up"
+    }
+    },10) 
+
+   
+  }
+ document.querySelector('.start')
+ .addEventListener('click',start) 
+
+ ////pause function///
+ function pause(){
+  clearInterval(intervalId);
+ }
+  document.querySelector('.pause')
+  .addEventListener('click',pause) 
+
+  ////reset function/// 
+  function reset(){
+    elapsed = 0;
+    clearInterval(intervalId);
+    timeDisplay.innerHTML = formateTime(elapsed)
+  } 
+  document.querySelector('.reset')
+  .addEventListener('click',reset)
+
+} 
+
+
+////stopwatch//// 
+
+function runningStopWatch(){
+  let elapsed = 0;
+  let intervalId;
+  let laps = [];
+////start function////
+  function start(){
+    intervalId = setInterval(()=>{
+      elapsed += 10;
+      stopwatchDisplay.innerHTML = `Stopwatch Time : ${formateTime(elapsed)}`
+    },10)
+  } 
+  document.querySelector('.start2')
+  .addEventListener('click',start)
  
-// mainDiv.innerHTML = `
-// <button class = "js-satrt">start</button>
-// <button class = "js-stop">Stop</button>`
-// let startStopWatch = document.querySelector('.js-satrt')
-// let stopStopWatch = document.querySelector('.js-stop') 
-// startStopWatch.addEventListener('click',()=>{
-//   intervalId2 = setInterval(()=>{
-//     let hrs = Math.floor(stopWatchTime / 3600000);
-//     let mins = Math.floor((stopWatchTime % 3600000) / 60000);
-//     let secs = Math.floor((stopWatchTime % 60000)/1000)
-//     let milisec = Math.floor((stopWatchTime % 1000)/10)
-//  stopWatchTime += 10
-//  h1.textContent = `${hrs}:${mins}:${secs}:${milisec}`
- 
-//   },10)
-// }) 
-// stopStopWatch.addEventListener('click',()=>{
-//   clearInterval(intervalId2)
-// })
-// })  
+ ////stop function////
+
+  function stop(){
+    clearInterval(intervalId);
+  } 
+  document.querySelector('.stop2')
+  .addEventListener('click',stop)
+
+////reset function////
+
+function reset(){
+  clearInterval(intervalId)
+  elapsed = 0;
+  stopwatchDisplay.innerHTML = `Stopwatch Time : ${formateTime(elapsed)}`
+} 
+document.querySelector('.reset').
+addEventListener('click',reset)
 
 
+////flag fucntion///
+ function flag(){
+  laps.push(formateTime(elapsed))
+  flagDisplay.innerHTML = laps
+  
+ } 
+ document.querySelector('.lap')
+ .addEventListener('click',flag)
 
+}
+
+
+render()
